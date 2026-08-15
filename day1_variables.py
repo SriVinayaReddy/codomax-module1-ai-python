206fe345b46fd788ffd8b59d9c107806b3b314b7
S.SrivinayaReddy <vinayareddy156@gmail.com>
2026-08-15 19:53:11 +0530

    Delete day1_variables.py

--- day1_variables.py
+++
@@ -1,8 +0,0 @@
-# Day 1: Variables & Data Types
-name = "Vinaya"
-age = 19
-is_student = True
-gpa = 8.5
-
-print(f"Name: {name}, Age: {age}, Student: {is_student}, GPA: {gpa}")
-print(type(name), type(age), type(is_student), type(gpa))
